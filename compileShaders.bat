5e6aacc82892ded5ecfb9f43f9c7f754e410f4cc
Devin <devin@hiitsdevin.dev>
2022-01-27 21:57:59 -0800

    Deleted `compileShaders.bat` in favor of `shaderCompiler.py`

--- compileShaders.bat
+++
@@ -1,2 +0,0 @@
-"C:\VulkanSDK\1.2.198.1\Bin\glslc.exe" src\shaders\simple_shader.vert -o src\shaders\simple_shader.vert.spv
-"C:\VulkanSDK\1.2.198.1\Bin\glslc.exe" src\shaders\simple_shader.frag -o src\shaders\simple_shader.frag.spv
